5419433f8c599784cd9de4ac9711db2aa8cb1e73
Yoon-jae <yoonjae.cho92@gmail.com>
2017-06-03 18:16:13 +0900

    Add API functions

---
+++ src/apibucket/mise.py
@@ -0,0 +1,37 @@
+#-*- coding: utf-8 -*-
+import sys
+import urllib
+import urllib2
+import json
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def get_mise(my_key, city):
+    convert_cityname = {'Seoul':u'서울','Busan':u'부산','Daegu':u'대구','Incheon':u'인천','Gwangju':u'광주','Daejeon':u'대전','Ulsan':u'울산','Gyeonggi':u'경기','Gangwon':u'강원','Chungbuk':u'충북','Chungnam':u'충남','Jeonbuk':u'전북','Jeonnam':u'전남','Gyeongbuk':u'경북','Gyeongnam':u'경남','Jeju':u'제주','Sejong':u'세종'}
+    convert_dust_level = {u'1':u'좋음',u'2':u'보통',u'3':u'나쁨',u'4':u'매우나쁨'}
+    apiKey = my_key
+    url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty"
+    sidoName = convert_cityname[city]
+    pageNo = "1"
+    numOfRows = "10"
+    version = "1.3"
+    returnType = "json"
+
+    params = urllib.urlencode({
+            'sidoName': sidoName,
+            'pageNo': pageNo,
+            'numOfRows': numOfRows,
+            'ver': version,
+            '_returnType': returnType
+    })
+   
+    params = params+"&ServiceKey="+apiKey
+
+    data = urllib2.urlopen(url, params).read()
+    data = json.loads(data)
+    
+    text = "현재 {city}의 미세먼지 농도는 {val} 이고, 상태는 {level}입니다.".format(city=sidoName, val=data["list"][1]["pm10Value"], level=convert_dust_level[data["list"][1]["pm10Grade"]])
+
+    print(text)
+    return text
---
+++ src/apibucket/weather.py
@@ -0,0 +1,36 @@
+#-*- coding: utf-8 -*-
+
+import json
+import requests
+
+sky_dict = {'SKY_D01': "맑은 날씨", 'SKY_D02': '구름이 조금 있는 날씨', 'SKY_D03': '구름이 많은 날씨', 'SKY_D04': '흐린 날씨', 'SKY_D05': '비가 오는 날씨', 'SKY_D06': '눈이 오는 날씨', 'SKY_D07': '비 또는 눈이 오는 날씨'}
+tomorrow_sky_dict = {'SKY_M01': "맑은 날씨", 'SKY_M02': '구름이 조금 있는 날씨', 'SKY_M03': '구름이 많은 날씨', 'SKY_M04': '흐린 날씨', 'SKY_M05': '비가 오는 날씨', 'SKY_M06': '눈이 오는 날씨', 'SKY_M07': '비 또는 눈이 오는 날씨'}
+
+def get_weather(my_key, lat, lon, time=0):
+	params = {"version":"1","lat":lat,"lon":lon}
+
+        key = {"appKey":my_key}
+
+	current_weather = requests.get("http://apis.skplanetx.com/weather/current/hourly",params=params, headers=key)
+	current_result = current_weather.json()
+
+	temperature = current_result['weather']['hourly'][0]['temperature']['tc']
+
+	summary_weather = requests.get("http://apis.skplanetx.com/weather/summary",params=params, headers=key)
+	result = summary_weather.json()
+
+	sky_code = result['weather']['summary'][0]['today']['sky']['code']
+	tmax = result['weather']['summary'][0]['today']['temperature']['tmax']
+	tmin = result['weather']['summary'][0]['today']['temperature']['tmin']
+
+	tomorrow_sky_code = result['weather']['summary'][0]['tomorrow']['sky']['code']
+	tomorrow_tmax = result['weather']['summary'][0]['tomorrow']['temperature']['tmax']
+	tomorrow_tmin = result['weather']['summary'][0]['tomorrow']['temperature']['tmin']
+
+	if time == 0:
+		text = "현재 온도는 {temp} 도 이고, 오늘의 최고 기온은 {tmax} 도, 최저 지온은 {tmin} 도 이며, {sky}입니다.".format(temp=temperature, tmax=tmax, tmin=tmin, sky=sky_dict[sky_code])
+	else:
+		text = "내일의 최고 기온은 {tmax} 도, 최저 지온은 {tmin} 도 이며, {sky}가 예상됩니다.".format(tmax=tomorrow_tmax, tmin=tomorrow_tmin, sky=tomorrow_sky_dict[tomorrow_sky_code])
+	
+	print(text)
+	return text
