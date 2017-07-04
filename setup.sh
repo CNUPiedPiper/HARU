62fbe7acec01e01f47837fee188c7206420e97f8
Yoonjae Cho <yoonjae.cho92@gmail.com>
2017-07-04 23:53:01 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get upgrade 
+apt-get install python-dev
+apt-get install ttf-unfonts-core
+apt-get install python-pyaudio python3-pyaudio sox
+apt-get install libatlas-base-dev libblas-dev liblapack-dev
+apt-get install gfortran
+pip install -U pip
+pip install -r requirements.txt
