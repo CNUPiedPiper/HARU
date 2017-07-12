c787b229a7b5a515dabfaf529f838ff29fbcf4d8
yoon-jae <yoonjae.cho92@gmail.com>
2017-07-13 01:12:04 +0900

    Edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 echo y | command
+
 apt-get update
 apt-get -y upgrade 
 apt-get install -y python-dev python-setuptools swig
@@ -6,8 +7,11 @@ apt-get install -y ttf-unfonts-core
 apt-get install -y python-pyaudio python3-pyaudio sox
 apt-get install -y libatlas-base-dev libblas-dev liblapack-dev
 apt-get install -y gfortran
+apt-get install -y build-essential scons
+
 pip install -U pip pip install -r requirements.txt
 pip install --upgrade gensim
+
 geo_data="./src/GeoLiteCity.dat"
 if [ -f "$geo_data" ]
 then
@@ -17,6 +21,7 @@ else
     wget -N http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
     gzip -d GeoLiteCity.dat.gz
 fi
+
 wiring_pi="./WiringPi-Python"
 if [ -f "$wiring_pi" ]
 then
@@ -31,4 +36,17 @@ else
     swig2.0 -python wiringpi.i
     sudo python setup.py install
 fi
+
+led_pi="./rpi_ws281x"
+if [ -f "$led_pi" ]
+then
+    echo "$led_pi already exist."
+else
+    git clone https://github.com/jgarff/rpi_ws281x.git
+    cd rpi_ws281x
+    scons
+    cd python
+    python setup.py install
+fi
+
 echo "Haru perfectly installed !" 
