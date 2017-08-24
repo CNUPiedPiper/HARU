d5d63490cfdd586bf0458d3ccf6c59b01e9f38b8
Yoonjae Cho <yoonjae.cho92@gmail.com>
2017-08-24 16:02:07 +0900

    Update get_dev_index.py

--- src/recorder/get_dev_index.py
+++ src/recorder/get_dev_index.py
@@ -1,6 +1,8 @@
 import pyaudio
 
 p = pyaudio.PyAudio()
+
+# Get audio device information
 for i in range(p.get_device_count()):
 	dev = p.get_device_info_by_index(i)
 	print((i, dev["name"], dev["maxInputChannels"]))
