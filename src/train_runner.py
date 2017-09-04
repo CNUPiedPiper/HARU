85253aeba89f2da9754988ad53cb99b178692a45
Yoonjae Cho <yoonjae.cho92@gmail.com>
2017-09-04 22:56:04 +0900

    Add comment on train_runner.py

--- src/train_runner.py
+++ src/train_runner.py
@@ -2,7 +2,7 @@ import sys
 from trainer import trainer
 import os.path
 
-
+# If you add a new function, use the follwing module to update the feature value which stored in model directory.
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print(os.path.dirname(__file__))
