8505aaa8b9522fb7ddf1cd2052185c86b44d239f
yoon-jae <yoonjae.cho92@gmail.com>
2017-11-21 17:13:49 +0900

    Implement long input error handling

--- src/recorder/recorder.py
+++ src/recorder/recorder.py
@@ -3,7 +3,8 @@ import wave
 from array import array
 import os
 import sys
-from speech_recognition import transcribe_streaming
+sys.path.append('/home/pi/prototype/src/speech_recognition')
+from transcribe_streaming import transcribe_streaming
 
 
 # Set device number with get_dev_index.py.
@@ -22,9 +23,11 @@ class Recorder:
 	def record_audio(self):
 		def is_silent(data):
 			# When it's silent, max(data) tend to be 100~200
-            result = max(data) < self.THRESHOLD
-            self.THRESHOLD = self.THRESHOLD + 0.3*(max(data) - self.THRESHOLD)
-			return result
+			#return max(data) < self.THRESHOLD
+			result = max(data) < self.THRESHOLD
+                self.THRESHOLD = self.THRESHOLD + 0.3 * (max(data) - self.THRESHOLD)
+                #print("THRESHOLD : " + str(self.THRESHOLD))
+                return result
 	
 		p = pyaudio.PyAudio()
 	
@@ -42,6 +45,7 @@ class Recorder:
 		frames = array('h')
 		minimum = 0
 		silent_counter = 0
+        MAX_FRAME_LENGTH = 230000
 		
 		while True:
 			data = array('h', stream.read(self.CHUNK))
@@ -53,6 +57,8 @@ class Recorder:
 
 			# After 3 seconds
 			else:
+                #print("SILENT COUNTER : " + str(silent_counter))
+                #print("FRAME LENGTH : " + str(len(frames)))
 				silent = is_silent(data)
 
 				# If it is noisy
@@ -67,6 +73,8 @@ class Recorder:
 
 				# After a second, finish the recording
 				else:
+                    if len(frames) > MAX_FRAME_LENGTH: 
+                        frames = frames[:MAX_FRAME_LENGTH]
 					break
 
 		print("[HARU] Recording is done")
