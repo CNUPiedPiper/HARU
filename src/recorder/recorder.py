7d9b9215f7b2312e2d7fbc2bcb391482f4feffdc
yoon-jae <yoonjae.cho92@gmail.com>
2017-11-21 17:20:27 +0900

    Fix indentation & import path

--- src/recorder/recorder.py
+++ src/recorder/recorder.py
@@ -3,9 +3,7 @@ import wave
 from array import array
 import os
 import sys
-sys.path.append('/home/pi/prototype/src/speech_recognition')
-from transcribe_streaming import transcribe_streaming
-
+from speech_recognition import transcribe_streaming
 
 # Set device number with get_dev_index.py.
 DEVICE_NUMBER = 1
@@ -23,11 +21,10 @@ class Recorder:
 	def record_audio(self):
 		def is_silent(data):
 			# When it's silent, max(data) tend to be 100~200
-			#return max(data) < self.THRESHOLD
 			result = max(data) < self.THRESHOLD
-                self.THRESHOLD = self.THRESHOLD + 0.3 * (max(data) - self.THRESHOLD)
-                #print("THRESHOLD : " + str(self.THRESHOLD))
-                return result
+            self.THRESHOLD = self.THRESHOLD + 0.3 * (max(data) - self.THRESHOLD)
+            #print("THRESHOLD : " + str(self.THRESHOLD))
+            return result
 	
 		p = pyaudio.PyAudio()
 	
