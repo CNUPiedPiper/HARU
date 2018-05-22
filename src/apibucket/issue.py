ffe69f1223f548cc34554b2aea740d612e64e094
Sumin Jegal <jegal.sumin@gmail.com>
2018-05-23 00:23:12 +0900

    Update issue.py
    Fixed an error that cannot get real-time issue keyword

--- src/apibucket/issue.py
+++ src/apibucket/issue.py
@@ -6,29 +6,17 @@ import urllib2
 from bs4 import BeautifulSoup
 
 def get_issue():
-    url = urllib2.urlopen('http://datalab.naver.com/keyword/realtimeList.naver?where=main')
+    url = urllib2.urlopen('https://www.naver.com')
     soup = BeautifulSoup(url, 'html.parser')
-    result = soup.find_all('ul')
-
+    result = soup.select(".PM_CL_realtimeKeyword_rolling span[class*=ah_k]")
+    
     issue_data = list()
     issue_data.extend([u"오늘의 이슈로는"])
-
-    for res in result:
-        if res['class'][0] == 'rank_list':
-            keywords = res.find_all('span')
-            top = 0
-
-            for key in keywords:
-                if top == 10:
-                    break
-
-                data = key.contents[0].replace(' ','')
-                issue_data.extend([unicode(data), ','])
-                top = top + 1
-
-            issue_data.extend([u"등 이있습니다."])
-
-            text = ' '.join(issue_data)
-
-            print('[HARU] {t}'.format(t=text))
-            return text 
+    
+    for idx, title in enumerate(result, 1):
+        issue_data.extend([unicode(title.text), ','])
+    
+    issue_data.extend([u"등 이있습니다."])
+    text = ' '.join(issue_data)
+    print('[HARU] {t}'.format(t=text))
+    return text
